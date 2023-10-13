fb0d72f3c8434b8f7e01b944d10ed6406077d9e7
Jisu Choi <redgem92@gmail.com>
2023-10-13 11:13:35 +0900

    REFACTOR: 자바 ORM 표준 JPA 프로그래밍 - JPA 시작
    - 리팩토링된 템플릿 추가

--- 010-자바-ORM-표준-JPA-프로그래밍/chapter02-jpa-시작/src/main/java/jpabook/jpastart/JpaMain.java
+++ 010-자바-ORM-표준-JPA-프로그래밍/chapter02-jpa-시작/src/main/java/jpabook/jpastart/JpaMain.java
@@ -1,45 +1,41 @@
 package jpabook.jpastart;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpaMain {
 
 	public static void main(String[] args) {
-        // EntityManager Factory - Create
-        // Create factory based on persistence.xml
-        // Find persistence unit from META-INF/persistence.xml named 'jpabook', then create entity manager factory.
+        // EntityManager Factory - 생성
+        // persistence.xml 설정 정보를 사용해 EntityManager Factory 생성해야 함
+        // 이때 Persistence 클래스 사용 -> EntityManager Factory를 생성해 JPA 사용하도록 함
+        //
+        // META-INF/persistence.xml에서 이름이 'jpabook'인 영속성 유닛(Persistence Unit)을 찾아 Factory 생성
+        // 이때 설정 정보를 읽어서 JPA 동작을 위한 기반 객체 생성 후, 구현체에 따라서 DB Connection Pool도 생성 -> 비용이 큶
+        // 따라서 EntityManager Factory는 애플리케이션 전체에서 한번만 생성하고 공유해서 사용해야 함
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
 
-        // EntityManager - Create
-        // Create manager from Factory
-        // It may CRUD entities using manager.
-        // Entity Manager connects with DB while maintaining datasource(DB Connection) internally.
+        // EntityManager - 생성
+        // Factory에서 EntityManager 생성
+        // JPA의 대부분 기능은 EntityManager가 제공 -> 엔티티를 DB에 CRUD
         //
-        // INFO:
-        // Since Entity Manager is firmly related with DB Connection, it must not be shared or reused among threads.
+        // INFO: EntityManager는 DB Connection과 밀접한 관계가 있으므로 Thread 간에 공유되거나 재사용하면 안됨
         EntityManager em = emf.createEntityManager();
 
-        // Transaction - Acquisition
-        EntityTransaction tx = em.getTransaction();
+        // Transaction - 관리
+        EntityTransaction tx = em.getTransaction(); // Transaction API
         try {
-            // Data must be changed in transaction if using JPA.
-            // If not, exception is thrown.
-            // If logic worked well, transaction is commit well.
-            // If exception is thrown, transaction needs to be rollback.
-            tx.begin();         // Transaction - Begin
+            // JPA 사용 시 반드시 Transaction 안에서 데이터 변경해야 함, 없이하면 예외 발생
+            tx.begin();         // Transaction - 시작
             logic(em);          // Logic
-            tx.commit();        // Transaction - Commit
+            tx.commit();        // Transaction - Commit(저장)
         } catch (Exception e) {
-            tx.rollback();      // Transaction - Rollback
+            tx.rollback();      // Transaction - 예외 발생 시 Rollback
         } finally {
-            // Entity Manager must be closed after use.
+            // 마지막으로 사용이 끝난 EntityManager는 반드시 종료
             em.close();
         }
-        // Factory either.
+        // Factory도 마찬가지
         emf.close();
     }
 
@@ -66,6 +62,23 @@ public class JpaMain {
 
         // Delete
         em.remove(member);
+    }
+
+    /**
+     * JPQL :
+     * JPA는 SQL을 추상화한 JPQL라는 객체지향 쿼리 언어를 제공
+     * SQL과 문법이 거의 유사해서 SELECT, FROM, WHERE, GROUP BY, HAVING, JOIN 등 사용 가능
+     *
+     * 차이점 :
+     * JPQL - 엔티티 객체, 쉽게 말해 클래스와 필드를 대상으로 쿼리
+     * SQL - DB 테이블 대상으로 쿼리
+     */
+    private void jpql(EntityManager em) {
+        // 쿼리 객체 생성
+        TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+
+        // 해당 메서드 호출을 통해 데이터 집합 -> 엔티티 컨테이너
+        List<Member> members = query.getResultList();
     }
 
 }
